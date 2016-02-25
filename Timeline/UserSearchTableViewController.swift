4c7e0f2723ce7f23268ee1d8fab80c042b50b172
Daniel Dickson <danieldickson89@gmail.com>
2016-02-25 08:25:02 -0700

    continuing part 2

--- Timeline/UserSearchTableViewController.swift
+++ Timeline/UserSearchTableViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class UserSearchTableViewController: UITableViewController {
+    
+    var userDataSource: [User] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
